0e4f746feb8399cdc6293d6d588d8d58db397d14
RyanSouza514 <ryansolza123@gmail.com>
2024-09-12 20:43:05 -0300

    fazendo rota do dash de mensagens

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\DashContatosController;
+use App\Http\Controllers\DashMensagens;
 use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +19,7 @@ Route::get('/', function () {
 Route::resource('/Dashbord/Produtos', ProdutosController::class);
 
 Route::post('/contato',[ContatoController::class, "contatoPost"])->name('contato');
+
+Route::get('/contato',[ContatoController::class, "contatoDash"])->name("contatoDash");
+
+Route::post('/dashbord/mensagens',[DashContatosController::class,'contatoDash'])->name('contato');
